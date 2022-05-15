ff7371b9061bfb47eb87a9fc45f3d552d2a540b4
Jsp_study <yumkl123@gmail.com>
2022-05-15 21:29:35 +0900

    반복문 연습문제1,2

--- LogicHomework/src/day7/view/Menu.java
+++ LogicHomework/src/day7/view/Menu.java
@@ -1,10 +1,55 @@
 package day7.view;
 
-import day7.controller.*;
+import java.util.Scanner;
 
 public class Menu {
+	Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Function fuc = new Function();
-		fuc.displayMenu();
+		Menu menu = new Menu();
+		menu.printStar2();
+	}
+	public void printStar1(){
+		System.out.println("정수 하나 입력하세요.");
+		int input = sc.nextInt();
+		
+		if (input >= 0) {
+			for (int i = 0; i < input; i++) {
+				for (int j = 0; j < i; j++) {
+					System.out.print("*");
+				}
+				System.out.println(i+1);
+			}
+		} else {
+			System.out.println("양수가 아닙니다.");
+		}
+	}
+	
+	public void printStar2(){
+		System.out.println("정수 하나 입력하세요.");
+		int input = sc.nextInt();
+		
+		if (input > 0) {
+			for (int i = 0; i < input; i++) {
+				for (int j = 0; j <= i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		} else if(input == 0) {
+			System.out.println("출력 기능이 없습니다.");
+		} else {
+			input = input * -1;
+			for (int i = input; i > 0; i--) {
+				for (int j = 0; j < input - i; j++) {
+					System.out.print(" ");
+				}
+				
+				for (int j = 0; j < i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
 	}
 }
