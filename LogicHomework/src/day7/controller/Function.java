b9b60a44af0dd4cb09233398cca1872de717ebb0
Jsp_study <yumkl123@gmail.com>
2022-05-15 17:40:26 +0900

    comparable, comparator

---
+++ LogicHomework/src/day7/controller/Function.java
@@ -0,0 +1,7 @@
+package day7.controller;
+
+public class Function {
+	public void displayMenu() {
+		System.out.println("displayMenu");
+	}
+}
---
+++ LogicHomework/src/day7/view/Menu.java
@@ -0,0 +1,10 @@
+package day7.view;
+
+import day7.controller.*;
+
+public class Menu {
+	public static void main(String[] args) {
+		Function fuc = new Function();
+		fuc.displayMenu();
+	}
+}
